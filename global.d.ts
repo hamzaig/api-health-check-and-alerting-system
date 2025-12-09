35d455757f41279c86299da812dbd31c72a94fd7
Hamza Ali Khalid <hamzaig@yahoo.com>
2025-12-10 04:28:35 +0500

    x

--- global.d.ts
+++ global.d.ts
@@ -5,11 +5,13 @@ declare global {
     conn: typeof mongoose | null;
     promise: Promise<typeof mongoose> | null;
   };
-  var __healthCheckWorker?: {
-    timer: NodeJS.Timeout | null;
-    running: boolean;
-    started: boolean;
-  };
+  var __healthCheckWorker:
+    | {
+        timer: NodeJS.Timeout | null;
+        running: boolean;
+        started: boolean;
+      }
+    | undefined;
 }
 
 export {};
